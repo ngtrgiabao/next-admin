364c0847a1479ee57827fab1829f5d41264ed1e1
ngtrgiabao <ngtrgiabao251002@gmail.com>
2024-03-22 17:47:49 +0700

    feat: add product detail page

--- app/dashboard/products/[id]/page.tsx
+++ app/dashboard/products/[id]/page.tsx
@@ -12,32 +12,30 @@ const SingleProductPage = () => {
       </div>
       <div className='flex-[3] bg-card p-5 rounded-lg'>
         <form action="" className='flex flex-col'>
-          <label className='text-sm'>Username</label>
-          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='text' name='username' placeholder='Yanji' />
+          <label className='text-sm'>Title</label>
+          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='text' name='text' placeholder='new product' />
 
-          <label className='text-sm'>Email</label>
-          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='email' name='email' placeholder='yanji@gmail.com' />
+          <label className='text-sm mt-2'>Price</label>
+          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='number' name='price' placeholder='24.99' />
 
-          <label className='text-sm'>Password</label>
-          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='password' name='password' placeholder='password' />
+          <label className='text-sm mt-2'>Stock</label>
+          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='number' name='stock' placeholder='12' />
 
-          <label className='text-sm'>Phone</label>
-          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='tel' name='phone' placeholder='+12222' />
+          <label className='text-sm mt-2'>Color</label>
+          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='text' name='color' placeholder='red' />
 
-          <label className='text-sm'>Address</label>
-          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='text' name='address' placeholder='New York' />
+          <label className='text-sm mt-2'>Color</label>
+          <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='text' name='size' placeholder='large' />
 
-          <label className='text-sm'>Is Admin ?</label>
-          <select className='p-2 border-2 border-[#2e374a] rounded-md my-2 bg-primary' name="isAdmin" id="isAdmin">
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-
-          <label className='text-sm'>Is Active ?</label>
+          <label className='text-sm mt-2'>Cat</label>
           <select className='p-2 border-2 border-[#2e374a] rounded-md my-2 bg-primary' name="isActive" id="isActive">
-            <option value="true">Yes</option>
-            <option value="false">No</option>
+            <option value="kitchen">Kitchen</option>
+            <option value="computer">Computers</option>
           </select>
+
+          <label className='text-sm mt-2'>Description</label>
+          <textarea name='desc' id="" cols={30} rows={10} className='my-2 bg-primary p-2 border-2 border-[#2e374a] rounded-md' placeholder='description'></textarea>
+
           <button className='w-full p-2 bg-teal-400 text-white cursor-pointer mt-5 border-none rounded-md'>Update</button>
         </form>
       </div>
--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -14,24 +14,31 @@ const SingleUserPage = () => {
         <form action="" className='flex flex-col'>
           <label className='text-sm'>Username</label>
           <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='text' name='username' placeholder='Yanji' />
-          <label className='text-sm'>Email</label>
+
+          <label className='text-sm mt-2'>Email</label>
           <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='email' name='email' placeholder='yanji@gmail.com' />
-          <label className='text-sm'>Password</label>
+
+          <label className='text-sm mt-2'>Password</label>
           <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='password' name='password' placeholder='password' />
-          <label className='text-sm'>Phone</label>
+
+          <label className='text-sm mt-2'>Phone</label>
           <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='tel' name='phone' placeholder='+12222' />
-          <label className='text-sm'>Address</label>
+
+          <label className='text-sm mt-2'>Address</label>
           <input className='p-2 border-2 border-[#2e374a] bg-primary rounded-md my-2' type='text' name='address' placeholder='New York' />
-          <label className='text-sm'>Is Admin ?</label>
+
+          <label className='text-sm mt-2'>Is Admin ?</label>
           <select className='p-2 border-2 border-[#2e374a] rounded-md my-2 bg-primary' name="isAdmin" id="isAdmin">
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>
-          <label className='text-sm'>Is Active ?</label>
+
+          <label className='text-sm mt-2'>Is Active ?</label>
           <select className='p-2 border-2 border-[#2e374a] rounded-md my-2 bg-primary' name="isActive" id="isActive">
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>
+
           <button className='w-full p-2 bg-teal-400 text-white cursor-pointer mt-5 border-none rounded-md'>Update</button>
         </form>
       </div>
